a1272bab65973994c0ae7e570bf94b793a3c54a1
wzee1 <108935996+wzee1@users.noreply.github.com>
2022-07-19 19:11:40 +0200

    Added Files

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// --------------
+// --------------
+// Hamburger Menu
+// --------------
+// --------------
+
+const hamburgerIcon = document.getElementById("hamburger");
+      const menu = document.getElementById("menu");
+      const navLinks = document.querySelectorAll(".nav__links");
+      const logo = document.getElementById("logo");
+
+// Toggles the menu
+hamburgerIcon.addEventListener("click", () => {
+   menu.classList.toggle("nav-active");
+})
+
+// Removes menu if a link is clicked
+navLinks.forEach(navLinks => {
+   navLinks.addEventListener("click", () => {
+      menu.classList.remove("nav-active");
+   });
+});
+
+// Removes menu if logo is clicked
+logo.addEventListener("click", () => {
+   menu.classList.remove("nav-active");
+})
+
+// -------------
+// -------------
+// Scroll Reveal
+// -------------
+// -------------
+
+ScrollReveal({
+   origin: 'top',
+   distance: '60px',
+   duration: 2000,
+   delay: 400,
+});
+
+ScrollReveal().reveal('.hero__text');
+
+ScrollReveal().reveal('.features__card',
+                     {delay: 500, origin: 'bottom'})
+
+ScrollReveal().reveal('.hero__illustration',
+            {delay: 1000, origin: 'right'});
+
+ScrollReveal().reveal('.bestSeller__text, .subOnly__image--container, .upComing__text, .signUp__image',
+            {delay: 500, origin: 'right'});
+
+ScrollReveal().reveal('.bestSeller__illustration, .subOnly__text, .upComing__image, .signUp__text',
+            {delay: 500, origin: 'left'});
+
+ScrollReveal().reveal('.subOnly__text::before, .subOnly__text::after, .signUp__text::before, signUp__text::after',
+         {delay: 1000, origin: 'left'});
